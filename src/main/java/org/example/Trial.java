757ba54ca6d3929a6edffec8adcbf423ee0b472b
Sidharth <sla@ext.uber.com>
2024-07-10 18:24:17 +0530

    Sid - TestCommit.02

--- src/main/java/org/example/Trial.java
+++ src/main/java/org/example/Trial.java
@@ -3,12 +3,7 @@ package org.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Trial {
     public static void main(String[] args) {
 
@@ -19,7 +14,6 @@ public class Trial {
         cops.setBinary("\\SIDHARTH\\New folder\\Google Chrome\\Chrome Browser\\Test Browser\\chrome-win64\\chrome.exe");
 
         WebDriver driver = new ChromeDriver(cops);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         System.out.println("Chrome opened");
 
         driver.get("https://www.washingtonpost.com/");
